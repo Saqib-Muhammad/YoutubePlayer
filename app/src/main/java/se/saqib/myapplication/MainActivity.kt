d0a278d28d694079c753d297713c3626920dfb3f
Muhammad Saqib <42799047+sesaqib@users.noreply.github.com>
2021-09-17 23:01:20 +0500

    Update MainActivity.kt

--- app/src/main/java/se/saqib/myapplication/MainActivity.kt
+++ app/src/main/java/se/saqib/myapplication/MainActivity.kt
@@ -25,23 +25,3 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         startActivity(intent)
     }
 }
-
-/*now the intent class has got several constructors,
-* but when you want to use to use an intent to launch
-* new activity
-*
-* YoutubeActivity::class.java
-* that's called a class literal,
-* and it's a way to pass a reference to a class as a parameter.
-* now you wouldn't pass string or integer as a parameter.
-* you'd use an actual string such as, you
-* know, tim in double quotes, or an actual number like 100,
-* and this is the same thing
-* we need to an object of type class,
-* whose value is the youtubeActivity class
-* and that's how you do it in kotlin
-*       just remember that when you want to pass a class
-* to a function, you have to create a class literal
-* by adding the colon class at the end
-* because these are java classes we're
-* also adding dot java*/
